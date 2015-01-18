730d0db879593d59ac458e935f5354d7fa6a56a7
Laurent Bernaille <laurent.bernaille@gmail.com>
2015-01-18 22:13:21 +0100

    Test of propertie file

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php
+echo '<p>Hello World</p>';
+$ini_array = parse_ini_file("application.properties");
+print_r($ini_array);
+?> 
  </body>
 </html>
