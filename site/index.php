e0bb66c1eedc2ef23db9c3f270dcaa30ec3c50c5
Laurent Bernaille <laurent.bernaille@gmail.com>
2015-01-19 00:30:42 +0100

    add php mysql, simple css and fix mysql connection

--- site/index.php
+++ site/index.php
@@ -6,9 +6,11 @@ use Aws\DynamoDb\DynamoDbClient;
 $props = parse_ini_file("application.properties");
 
 # Initialize DB connection
-$link = mysql_connect($props['dblocation'], $props['dbuser'], $props['dbpwd']);
-if (!$link) { die('Could not connect: ' . mysql_error()); }
-$selected = mysql_select_db("examples",$props['database']) or die("Could not select examples");
+$conn = new mysqli($props['dbhost'], $props['dbuser'], $props['dbpwd'],$props['database'],$props['dbport']);
+// check connection
+if ($conn->connect_error) {
+  trigger_error('Database connection failed: '  . $conn->connect_error, E_USER_ERROR);
+}
 
 # Initialize DynamoDB client
 $client = DynamoDbClient::factory(array( 'profile' => 'default', 'region' => $props['region']));
@@ -17,19 +19,28 @@ $client = DynamoDbClient::factory(array( 'profile' => 'default', 'region' => $pr
 echo "<html>\n";
 echo " <head>\n";
 echo "  <title>Automation Demo</title>\n";
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"demo.css\">\n";
 echo " </head>\n";
 echo " <body>\n";
 
-$result = mysql_query("show tables;");
-echo "<p>Database content>\n";
-echo "Tables: <b>$result<b>";
+
+echo "<p><h3>Database content</h3>\n";
+$rs=$conn->query("show tables");
+if($rs === false) {
+  trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+} else {
+  $rs->data_seek(0);
+  while($row = $rs->fetch_row()){
+    echo $row[0]."<br/>";
+  }
+}
 echo "</p>\n";
 
 
+echo "<p><h3>DynamoDB content</h3>";
 $iterator = $client->getIterator('Scan', array( 'TableName' => 'AMI_artifacts'));
-echo "<p>DynamoDB content\n";
-echo "<table>\n";
-echo "<tr><th>Application</th><th>Build ID</th><th>AMI</th></tr>\n";
+echo "<table class='t1'>\n";
+echo "<thead><tr><th>Application</th><th>Build ID</th><th>AMI</th></tr></thead>\n";
 foreach ($iterator as $item) {
   echo '<tr><td>'.$item['Application']['S'].'</td><td>'.$item['Build_ID']['S'].'</td><td>'.$item['AMI']['S']."</td></tr>\n";
 }
@@ -40,4 +51,4 @@ echo " </body>\n";
 echo "</html>\n";
 
 mysql_close($link);
-?> 
+?>
