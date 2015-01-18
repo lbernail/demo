4e7d249ee2597080f6e712eb63cd43306bd3aa39
Laurent Bernaille <laurent.bernaille@gmail.com>
2015-01-18 23:27:56 +0100

    Manage sdk dependencies, add configuration sample and some data retrieval

--- index.php
+++ index.php
@@ -1,12 +1,43 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php
-echo '<p>Hello World</p>';
-$ini_array = parse_ini_file("application.properties");
-print_r($ini_array);
+<?php
+require 'vendor/autoload.php';
+use Aws\DynamoDb\DynamoDbClient;
+
+
+$props = parse_ini_file("application.properties");
+
+# Initialize DB connection
+$link = mysql_connect($props['dblocation'], $props['dbuser'], $props['dbpwd']);
+if (!$link) { die('Could not connect: ' . mysql_error()); }
+$selected = mysql_select_db("examples",$props['database']) or die("Could not select examples");
+
+# Initialize DynamoDB client
+$client = DynamoDbClient::factory(array( 'profile' => 'default', 'region' => $props['region']));
+
+
+echo "<html>\n";
+echo " <head>\n";
+echo "  <title>Automation Demo</title>\n";
+echo " </head>\n";
+echo " <body>\n";
+
+$result = mysql_query("show tables;");
+echo "<p>Database content>\n";
+echo "Tables: <b>$result<b>";
+echo "</p>\n";
+
+
+$iterator = $client->getIterator('Scan', array( 'TableName' => 'AMI_artifacts'));
+echo "<p>DynamoDB content\n";
+echo "<table>\n";
+echo "<tr><th>Application</th><th>Build ID</th><th>AMI</th></tr>\n";
+foreach ($iterator as $item) {
+  echo '<tr><td>'.$item['Application']['S'].'</td><td>'.$item['Build_ID']['S'].'</td><td>'.$item['AMI']['S']."</td></tr>\n";
+}
+echo "</table>\n";
+echo "</p>\n";
+
+echo " </body>\n";
+echo "</html>\n";
+
+mysql_close($link);
 ?> 
- </body>
-</html>
