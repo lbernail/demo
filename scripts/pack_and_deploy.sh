ca46c636f5fa45ac52631ed60fde7e8117fcc2a6
Laurent Bernaille <laurent.bernaille@gmail.com>
2016-02-08 18:23:39 +0100

    configure packer

--- scripts/pack_and_deploy.sh
+++ scripts/pack_and_deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Hello"
-pip freeze
-aws s3 ls
+BUILD_DIR=$1
+PACKER_VARS=$2
+COMMIT=$3
+
+packer build -var-file $BUILD_DIR/$PACKER_VARS -var "source_dir=$BUILD_DIR" -var "commit=${COMMIT:0:7}" $BUILD_DIR/packer_apache_php.json
