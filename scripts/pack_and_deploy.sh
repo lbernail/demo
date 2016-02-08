a05133614f89a71860fb50edd4202028e4e30a29
Laurent Bernaille <laurent.bernaille@gmail.com>
2016-02-08 20:39:12 +0100

    New directories, small fixes

--- scripts/pack_and_deploy.sh
+++ scripts/pack_and_deploy.sh
@@ -3,6 +3,7 @@ set -e
 
 BUILD_DIR=$1
 PACKER_VARS=$2
-COMMIT=$3
+PACKER_TEMPLATE=$3
+COMMIT=$4
 
-packer build -var-file $BUILD_DIR/$PACKER_VARS -var "source_dir=$BUILD_DIR" -var "commit=${COMMIT:0:7}" $BUILD_DIR/packer_apache_php.json
+packer build -var-file $BUILD_DIR/$PACKER_VARS -var "source_dir=$BUILD_DIR" -var "commit=${COMMIT:0:7}" $BUILD_DIR/$PACKER_TEMPLATE
