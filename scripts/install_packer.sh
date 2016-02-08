908b7d3d9e6d3bdf6b2f1228e119f04a505492a5
Laurent Bernaille <laurent.bernaille@gmail.com>
2016-02-08 11:59:32 +0100

    With script to install packer

---
+++ scripts/install_packer.sh
@@ -0,0 +1,16 @@
+set -e
+
+PACKER_DL=${TOOLS_DIR}/packer-${PACKER_VERSION}
+
+mkdir -p ${TOOLS_DIR}/bin      
+                               
+if [ ! -d "${PACKER_DL}" ]
+then
+    mkdir -p ${PACKER_DL}
+    wget -q -O packer-${PACKER_VERSION}.zip https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+    unzip -q packer-${PACKER_VERSION}.zip -d ${PACKER_DL}
+    rm packer-${PACKER_VERSION}.zip
+fi
+
+ln -sf ${PACKER_DL}/packer ${TOOLS_DIR}/bin
+packer version
