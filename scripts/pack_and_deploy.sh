af3f0dbb393fb15ff76980c84520c0c2e3613e91
Laurent Bernaille <laurent.bernaille@gmail.com>
2016-02-09 10:39:53 +0100

    simplify security groups, use force option, retrieve generated ami

--- scripts/pack_and_deploy.sh
+++ scripts/pack_and_deploy.sh
@@ -6,4 +6,7 @@ PACKER_VARS=$2
 PACKER_TEMPLATE=$3
 COMMIT=$4
 
-packer build -var-file $BUILD_DIR/$PACKER_VARS -var "source_dir=$BUILD_DIR" -var "commit=${COMMIT:0:7}" $BUILD_DIR/$PACKER_TEMPLATE
+packer build -force -var-file $BUILD_DIR/$PACKER_VARS -var "source_dir=$BUILD_DIR" -var "commit=${COMMIT:0:7}" $BUILD_DIR/$PACKER_TEMPLATE | tee /tmp/packer.out
+
+ami=$(egrep "(eu|us|ap|sa)-(west|central|east|northeast|southeast)-(1|2): ami-" /tmp/packer.out | sed 's/.*ami-\([0-9a-f]*\).*/ami-\1/g')
+echo $ami
