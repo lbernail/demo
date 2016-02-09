ca3ffa2107197225bebdef72e7ac21fff5f4c821
Laurent Bernaille <laurent.bernaille@gmail.com>
2016-02-09 10:58:16 +0100

    clean and reorganize directories

--- scripts/pack_and_deploy.sh
+++ scripts/pack_and_deploy.sh
@@ -6,7 +6,9 @@ PACKER_VARS=$2
 PACKER_TEMPLATE=$3
 COMMIT=$4
 
-packer build -force -var-file $BUILD_DIR/$PACKER_VARS -var "source_dir=$BUILD_DIR" -var "commit=${COMMIT:0:7}" $BUILD_DIR/$PACKER_TEMPLATE | tee /tmp/packer.out
+PACKER_DIR=$BUILD_DIR/packer
+
+packer build -force -var-file $PACKER_DIR/$PACKER_VARS -var "site_dir=$BUILD_DIR/site" -var "commit=${COMMIT:0:7}" $PACKER_DIR/$PACKER_TEMPLATE | tee /tmp/packer.out
 
 ami=$(egrep "(eu|us|ap|sa)-(west|central|east|northeast|southeast)-(1|2): ami-" /tmp/packer.out | sed 's/.*ami-\([0-9a-f]*\).*/ami-\1/g')
 echo $ami
