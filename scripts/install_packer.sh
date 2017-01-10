2ebdcb16c34d7c5df323bf2a2127bb7001bbf549
Laurent Bernaille <laurent.bernaille@gmail.com>
2017-01-10 15:59:30 +0100

    debug packer

--- scripts/install_packer.sh
+++ scripts/install_packer.sh
@@ -9,11 +9,11 @@ if [ ! -d "${PACKER_DL}" ]
 then
     mkdir -p ${PACKER_DL}
     wget -q -O packer-${PACKER_VERSION}.zip https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+    ls -l ${PACKER_DL}
+    unzip
     unzip -q packer-${PACKER_VERSION}.zip -d ${PACKER_DL}
     rm packer-${PACKER_VERSION}.zip
 fi
-
-ls -l ${PACKER_DL}
 
 ln -sf ${PACKER_DL}/packer ${TOOLS_DIR}/bin
 packer version
