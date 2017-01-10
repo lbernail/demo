8bf8f5801d8a5ef9bafbfb4756b4c2aa28d0d14b
Laurent Bernaille <laurent.bernaille@gmail.com>
2017-01-10 16:26:13 +0100

    replace wget with curl

--- scripts/install_packer.sh
+++ scripts/install_packer.sh
@@ -8,8 +8,9 @@ mkdir -p ${TOOLS_DIR}/bin
 if [ ! -d "${PACKER_DL}" ]
 then
     mkdir -p ${PACKER_DL}
-    wget -O /tmp/packer-${PACKER_VERSION}.zip http://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
-    unzip /tmp/packer-${PACKER_VERSION}.zip -d ${PACKER_DL}
+    #wget -O /tmp/packer-${PACKER_VERSION}.zip http://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+    curl -sSLo packer-${PACKER_VERSION}.zip  https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+    unzip packer-${PACKER_VERSION}.zip -d ${PACKER_DL}
     rm packer-${PACKER_VERSION}.zip
 fi
 
