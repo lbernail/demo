3e5fa71c9c2d54ba65ac0d1041144148fda7fee2
Laurent Bernaille <laurent.bernaille@gmail.com>
2017-01-10 16:55:01 +0100

    fix typo

--- scripts/install_packer.sh
+++ scripts/install_packer.sh
@@ -2,7 +2,6 @@
 set -x
 set -e
 
-openssl version
 PACKER_DL=${TOOLS_DIR}/packer-${PACKER_VERSION}
 
 mkdir -p ${TOOLS_DIR}/bin      
@@ -10,7 +9,7 @@ mkdir -p ${TOOLS_DIR}/bin
 if [ ! -d "${PACKER_DL}" ]
 then
     mkdir -p ${PACKER_DL}
-    wget -O /tmp/packer-${PACKER_VERSION}.zip http://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+    wget -O packer-${PACKER_VERSION}.zip http://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
     unzip packer-${PACKER_VERSION}.zip -d ${PACKER_DL}
     rm packer-${PACKER_VERSION}.zip
 fi
